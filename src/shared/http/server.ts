a90b720f66c0d65babeebc5f84677074e7b5c4c2
Leonardo Natal <leonardo_natal93@hotmail.com>
2021-07-09 17:29:35 -0300

    rate limiter

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -4,38 +4,41 @@ import uploaConfig from '@config/upload';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
-import {errors} from 'celebrate';
+import { errors } from 'celebrate';
 // import {pagination} from 'typeorm-pagination';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/container';
 import '@shared/typeorm';
 import { PassThrough } from 'node:stream';
-
+import rateLimiter from '@shared/http/middlewares/rateLimiter';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(rateLimiter);
 // app.use(pagination);
 app.use('/files', express.static(uploaConfig.directory));
 app.use(routes);
 app.use(errors());
 
 //Error middleware
-app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
-  console.log('ERRO EXT',error);
-  if (error instanceof AppError) {
-    return response.status(error.statusCode).json({
-      status: 'error',
-      message: error.message,
-    });
-  }
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    console.log('ERRO EXT', error);
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
 
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
-  });
-});
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
