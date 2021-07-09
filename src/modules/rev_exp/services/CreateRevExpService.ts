c446c9c4f57e5f2badef3c080692136bef6a5883
Leonardo Natal <leonardo_natal93@hotmail.com>
2021-07-09 17:04:44 -0300

    iniciando com redis cache

--- src/modules/rev_exp/services/CreateRevExpService.ts
+++ src/modules/rev_exp/services/CreateRevExpService.ts
@@ -1,4 +1,4 @@
-
+import RedisCache from '@shared/cache/RedisCache';
 import AppError from '@shared/errors/AppError';
 import { injectable, inject } from 'tsyringe';
 import RevExp from '../typeorm/entities/RevExp';
@@ -15,13 +15,17 @@ interface IRequest {
 class CreateRevExpService {
   constructor(
     @inject('RevExpRepository')
-    private revExpRepository: RevExpRepository
-    ) {};
+    private revExpRepository: RevExpRepository,
+  ) {}
 
   public async execute(revExp: IRequest, userId: string): Promise<RevExp> {
-    revExp.user_id = userId;
-    return await this.revExpRepository.create(revExp as RevExp);
+    const redisCache = new RedisCache();
+    await redisCache.invalidate('revexp');
 
+    revExp.user_id = userId;
+    const rexp = await this.revExpRepository.create(revExp as RevExp);
+
+    return rexp;
   }
 }
 
--- src/modules/rev_exp/services/ListRevExpService.ts
+++ src/modules/rev_exp/services/ListRevExpService.ts
@@ -1,17 +1,36 @@
-
-import { paginateRawCustom, RequestPagination } from '@shared/typeorm/CustomPaginationMeta';
+import {
+  paginateRawCustom,
+  RequestPagination,
+} from '@shared/typeorm/CustomPaginationMeta';
 import { injectable, inject } from 'tsyringe';
 import RevExpRepository from '../typeorm/repositories/RevExpRepository';
+import RedisCache from '@shared/cache/RedisCache';
 
 @injectable()
 class ListRevExpService {
   constructor(
     @inject('RevExpRepository')
-    private revExpRepository: RevExpRepository
-    ) {};
+    private revExpRepository: RevExpRepository,
+  ) {}
 
-  public async execute(userId: string, paginate?: RequestPagination): Promise<any> {
-    const revExp = await paginateRawCustom(this.revExpRepository.findCategories(userId), paginate);
+  public async execute(
+    userId: string,
+    paginate?: RequestPagination,
+  ): Promise<any> {
+    const redisCache = new RedisCache();
+
+    let revExp = await redisCache.recover('revexp');
+
+    if (!revExp) {
+      revExp = await paginateRawCustom(
+        this.revExpRepository.findCategories(userId),
+        paginate,
+      );
+
+      await redisCache.save('revexp', revExp);
+    }
+
+    await redisCache.save('teste', 'teste');
 
     return revExp;
   }
